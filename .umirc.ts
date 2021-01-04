e8c7918940ccc332cda30203af4bf35e841c1e69
maolizhen <i-maolizhen@shuwen.com>
2021-01-04 10:46:20 +0800

    feat: :tada: init

---
+++ .umirc.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'lz-hook',
+  // favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  // logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  // outputPath: 'docs-dist',
+  // mode: 'site',
+  // // more config: https://d.umijs.org/config
+  dynamicImport: {},
+  resolve: {
+    includes: [
+      'src'
+    ]
+  },
+  extraBabelPlugins: [
+    [
+      'babel-plugin-import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+      },
+      'antd',
+    ]
+  ]
+});
