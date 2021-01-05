f15cfbd157da700588d745f4753eff234d3b942a
maolizhen <i-maolizhen@shuwen.com>
2021-01-05 12:09:56 +0800

    fix: useStorage Demo

--- src/useStorage/demo/index.tsx
+++ src/useStorage/demo/index.tsx
@@ -4,26 +4,29 @@ import { useStorage } from 'lz-hooks';
 
 const Demo: React.FC = () => {
   const [info, setInfo, removeInfo] = useStorage('info', 'session');
-
-  const onSave = () => {
-    const info = {
-      name: 'mlz',
-      age: 18,
-    };
-
-    setInfo(info);
-  };
-
-  const onRemove = () => {
-    removeInfo();
+  const [localInfo, setLocalInfo, removeLocalInfo] = useStorage(
+    'info',
+    'local',
+  );
+  const data = {
+    name: 'mlz',
+    age: 18,
   };
 
   return (
     <>
-      <Button onClick={onSave}>点击存储数据</Button>
-      <Button onClick={onRemove} style={{ margin: '0 16px' }}>
-        点击删除数据
-      </Button>
+      <>
+        <Button onClick={() => setInfo(data)}>session存储数据</Button>
+        <Button onClick={() => removeInfo()} style={{ margin: '0 16px' }}>
+          session删除数据
+        </Button>
+      </>
+      <>
+        <Button onClick={() => setLocalInfo(data)}>local存储数据</Button>
+        <Button onClick={() => removeLocalInfo()} style={{ margin: '0 16px' }}>
+          local删除数据
+        </Button>
+      </>
     </>
   );
 };
