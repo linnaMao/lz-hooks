9d60e98158cd212d1a27b6667bfde98756196cf7
maolizhen <i-maolizhen@shuwen.com>
2021-01-08 15:06:06 +0800

    feat: config

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,3 +4,4 @@ export { default as useToggle } from './useToggle';
 export { default as useStorage } from './useStorage';
 export { default as useCbState } from './useCbState';
 export { default as useInterval } from './useInterval';
+export { default as useReactive } from './useReactive';
