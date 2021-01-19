5632b727b13803cf0af055647ccf027077e59f45
maolizhen <i-maolizhen@shuwen.com>
2021-01-19 14:43:12 +0800

    fix: :wrench: modify configuration item

--- .umirc.ts
+++ .umirc.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'lz-hook',
+  description: 'hahah',
   // logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
@@ -9,9 +10,6 @@ export default defineConfig({
   mode: 'site',
   // more config: https://d.umijs.org/config
   dynamicImport: {},
-  resolve: {
-    includes: ['src'],
-  },
   extraBabelPlugins: [
     [
       'babel-plugin-import',
